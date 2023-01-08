17570cc727175949f3ff5f5ad3b040edeb8d787f
Niko <ks2225@protonmail.com>
2023-01-08 22:10:42 +0200

    Feat: spice up the tool-tips

--- src/site/twitch.tv/modules/chat/components/ChatEmoteTooltip.vue
+++ src/site/twitch.tv/modules/chat/components/ChatEmoteTooltip.vue
@@ -1,20 +1,76 @@
 <template>
 	<div ref="tooltip" class="seventv-tooltip" tooltip-type="emote">
-		<h3>{{ emote.name }}</h3>
+		<img ref="imgRef" class="tooltip-emote" :srcset="srcSet" :alt="emote.name" />
+		<h3 class="emote-name">{{ emote.name }}</h3>
+		<Logo class="logo" :provider="emote.provider" />
 	</div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
-	emote: SevenTV.Emote;
-}>();
+import { computed } from "vue";
+import Logo from "@/assets/svg/Logo.vue";
+
+const props = withDefaults(
+	defineProps<{
+		emote: SevenTV.ActiveEmote;
+		imageFormat?: SevenTV.ImageFormat;
+		unload?: boolean;
+	}>(),
+	{ unload: false, imageFormat: "WEBP" },
+);
+
+const srcSet = computed(() => (props.unload ? "" : getSrcSet(props.emote)));
+
+function getSrcSet(emote: SevenTV.ActiveEmote) {
+	const host = emote.data?.host ?? { url: "", files: [] };
+	const format = host.files.some((fi) => fi.format === props.imageFormat) ? props.imageFormat : host.files[0]?.format;
+
+	return host.files
+		.filter((f) => f.format === format)
+		.map((f, i) => `${host.url}/${f.name} ${i - 1}x`)
+		.join(", ");
+}
 </script>
 
 <style scoped lang="scss">
 .seventv-tooltip {
-	background-color: rgba(0, 0, 0, 0.65%);
-	backdrop-filter: blur(0.25em);
-	border-radius: 0.33em;
-	padding: 1em;
+	display: grid;
+	min-width: 12em;
+	background-color: var(--seventv-background-transparent-2);
+	backdrop-filter: blur(16px);
+	outline: 1px solid var(--seventv-border-transparent-1);
+	border-radius: 12px;
+	padding: 1.5em;
+}
+.emote-name {
+	grid-row-start: 2;
+	grid-column-start: 1;
+	font-size: 16px;
+	font-weight: 300;
+	max-width: 300px;
+	word-break: break-all;
+	justify-self: start;
+	align-self: center;
+}
+
+.logo {
+	grid-row-start: 2;
+	grid-column-start: 2;
+	align-self: end;
+	justify-self: center;
+	margin-left: 2em;
+	width: 18px;
+	height: auto;
+}
+.tooltip-emote {
+	grid-row-start: 1;
+	grid-column-start: 1;
+	grid-column-end: 3;
+	justify-self: center;
+	align-self: center;
+	padding-top: 1.5em;
+	padding-bottom: 3em;
+	height: auto;
+	max-width: 100px;
 }
 </style>
