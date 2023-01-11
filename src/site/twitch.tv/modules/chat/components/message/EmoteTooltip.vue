3f103dc1d7cf94382077871828c2e50111654327
Anatole <me@anatole.dev>
2023-01-11 07:50:21 +0100

    Additional tooltip context info

--- src/site/twitch.tv/modules/chat/components/message/EmoteTooltip.vue
+++ src/site/twitch.tv/modules/chat/components/message/EmoteTooltip.vue
@@ -1,16 +1,32 @@
 <template>
 	<div ref="tooltip" class="seventv-tooltip" tooltip-type="emote">
 		<img ref="imgRef" class="tooltip-emote" :srcset="srcSet" :alt="emote.name" />
-		<div class="spacer">
+
+		<div class="details">
 			<h3 class="emote-name">{{ emote.name }}</h3>
 			<div class="spacer"></div>
 			<Logo class="logo" :provider="emote.provider" />
+		</div>
+
+		<!-- Creator -->
+		<div v-if="emote.data?.owner" class="creator-label">
+			by
+			<span class="creator-name">{{ emote.data.owner.display_name }}</span>
+		</div>
+
+		<!-- Labels -->
+		<div class="labels">
+			<div v-if="isGlobal" class="label-global">Global Emote</div>
+			<div v-if="isSubscriber" class="label-subscriber">Subscriber Emote</div>
+			<div v-if="isChannel" class="label-channel">Channel Emote</div>
+			<div v-if="isPersonal" class="label-personal">Personal Emote</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
+import { DecimalToStringRGBA } from "@/common/Color";
 import { imageHostToSrcset } from "@/common/Image";
 import Logo from "@/assets/svg/logos/Logo.vue";
 
@@ -28,43 +44,72 @@ const props = withDefaults(
 const srcSet = computed(() => (props.unload ? "" : imageHostToSrcset(props.emote.data!.host, props.imageFormat)));
 const width = computed(() => `${props.width * 3}px`);
 const height = computed(() => `${props.height * 3}px`);
+
+const isGlobal = computed(() => props.emote.scope === "GLOBAL");
+const isSubscriber = computed(() => props.emote.scope === "SUB");
+const isChannel = computed(() => props.emote.scope === "CHANNEL");
+const isPersonal = computed(() => props.emote.scope === "PERSONAL");
+
+const creatorColor = computed(() => {
+	if (!props.emote.data || !props.emote.data.owner) return "inherit";
+	if (!props.emote.data.owner.style?.color) return "inherit";
+
+	return DecimalToStringRGBA(props.emote.data.owner.style.color);
+});
 </script>
 
 <style scoped lang="scss">
 .seventv-tooltip {
 	display: flex;
 	flex-direction: column;
-	row-gap: 1em;
 	align-items: center;
-	max-width: 12em;
-	background-color: var(--seventv-background-transparent-2);
-	backdrop-filter: blur(1em);
-	outline: 0.1em solid var(--seventv-border-transparent-1);
-	border-radius: 0.25em;
-	padding: 1.3em;
+	max-width: 21em;
+	padding: 0.5em 1.15em;
 }
+
 .emote-name {
 	font-size: 1.5rem;
 	font-weight: 300;
-	max-width: 18em;
+	max-width: 21em;
 	word-break: break-all;
 	float: left;
 }
+
 .logo {
-	width: 1.6em;
+	width: 2rem;
 	height: auto;
 	float: right;
 	align-self: end;
 }
-.tooltip-emote {
+
+img.tooltip-emote {
+	margin-bottom: 1rem;
 	width: v-bind("width");
 	height: v-bind("height");
 }
-.spacer {
+
+.details {
 	display: flex;
-	column-gap: 0.25em;
+	column-gap: 0.25rem;
 	flex: 1;
-	align-items: center;
-	justify-items: stretch;
+
+	> h3 {
+		font-weight: 600;
+	}
+}
+
+.creator-label {
+	font-size: 0.9rem;
+
+	.creator-name {
+		color: v-bind("creatorColor");
+	}
+}
+
+.labels {
+	font-size: 0.9rem;
+	> .label-global {
+		color: rgb(70, 220, 100);
+	}
 }
 </style>
